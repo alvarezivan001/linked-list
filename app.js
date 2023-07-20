cd390eeb5dce4ece49a40d08068572ab02f99d95
Ivan Alvarez <alvarezivan001@yahoo.com>
2023-07-20 16:14:44 -0700

    finished functions

--- app.js
+++ app.js
@@ -26,9 +26,21 @@ class LinkedList {
         this.head.nextNode = temp;
     }
     size(){
+        let i = 1;
+        let currentNode = this.head;
+        while(currentNode.nextNode != null)
+        {
+            i++;
+            currentNode = currentNode.nextNode;
+        }
+
+        console.log("size: " + i);
+        return i;
 
     }
-    head(){
+    whatishead(){
+        console.log(this.head + " warning");
+        return this.head;
         
     }
     tail(){
@@ -37,32 +49,117 @@ class LinkedList {
         {
             currentNode = currentNode.nextNode;
         }
+        console.log(currentNode);
         return currentNode;
     }
     at(index) {
-
+        let currentNode = this.head;
+        while(index > 0)
+        {
+            currentNode = currentNode.nextNode;
+            index--;
+        }
+        console.log(currentNode);
+        return currentNode;
     }
     pop(){
-        
+        let currentNode = this.head;
+        let prevNode = null;
+        while(currentNode.nextNode != null)
+        {
+            prevNode = currentNode;
+            currentNode = currentNode.nextNode;
+        }
+        prevNode.nextNode = null;
     }
     contains(value){
-
+        let currentNode = this.head;
+        let yes = false;
+        currentNode.value == value ? yes = true : false;
+        while(yes == false && currentNode != null)
+        {
+            
+            currentNode.value == value ? yes = true : false;
+            currentNode = currentNode.nextNode;
+        }
+        console.log(yes);
+        return yes;
     }
     find(value){
+        let i = 0;
+        let no = true;
+        let currentNode = this.head;
+        currentNode.value == value ? no = false : currentNode = currentNode.nextNode;
+        while(no == true && currentNode != null)
+        {
+            i++;
+            currentNode.value == value ? no = false: true;
+            currentNode = currentNode.nextNode;
+        }
+        if(no == true){console.log(false); return null;}
+        else {console.log(i); return i;}
         
     }
     toString(){
         //fix ouput
         let currentNode = this.head;
-        do{
-            console.log("( " + currentNode.value + " ) -> ");
+        while(currentNode.nextNode != null){
+            process. stdout. write("( " + currentNode.value + " ) -> ");
             if(currentNode.nextNode != null) 
             {currentNode = currentNode.nextNode;}
             else
-            {console.log("null");}
+            { process. stdout. write("null");}
             
-        }while(currentNode.nextNode != null)
-        
+        }
+
+        process. stdout. write("( " + currentNode.value + " ) -> ");
+        console.log("null");
+    }
+    insertAt(value, index){
+        if(index <= 0) {
+            console.log("inserting as head");
+            this.prepend(value);
+        }
+        else if(index > (this.size()-1))
+        {
+            console.log("out/end of range, appending to tail");
+            this.append(value);
+        }
+        else{
+            let i = 0;
+            let currentNode = this.head;
+            let prevNode = null;
+            let newNode = new Node(value);
+            while(i < index && currentNode.nextNode != null)
+            {
+                prevNode = currentNode;
+                currentNode = currentNode.nextNode;
+                i++;
+            }
+            newNode.nextNode = currentNode;
+            prevNode.nextNode = newNode;
+        }
+    }
+    removeAt(index){
+        if(index >= (this.size()-1))
+        {
+            console.log("out/end of range, removing tail");
+            this.pop();
+       }
+       else{
+            let i = 0;
+            let prevNode = null;
+            let currentNode = this.head;
+            
+            while(i < index)
+            {
+                prevNode = currentNode;
+                currentNode = currentNode.nextNode;
+                i++;
+            }
+            currentNode = currentNode.nextNode;
+            prevNode.nextNode = currentNode;
+       }
     }
 };
 
@@ -80,7 +177,12 @@ class Node {
 let newList = new LinkedList(new Node(1));
 newList.append(5);
 newList.append(3);
-newList.append(1);
-newList.append(9);
+newList.prepend(9);
+newList.prepend(7);
+newList.toString();
+// newList.pop();
 
-newList.toString();//fix ouput
+// newList.insertAt(4,5);
+newList.removeAt(4);
+
+newList.toString();
